727dcbd089b91b2f64841cd2158e4e6e8174a9b5
SchoolPC <>
2018-05-03 09:52:17 +0300

    Initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What would you like to do?:");
+            Console.WriteLine("(1) Write a note.");
+            Console.WriteLine("(2) Read all the current notes.");
+            var answer = Console.ReadLine();
+
+            if (answer == "1")
+            {
+
+            }
+            if (answer == "2")
+            {
+
+            }
+        }
+    }
+}
